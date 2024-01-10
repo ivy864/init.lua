4ab644d32024abd48cf0a159605b98d2213de3d9
vayers <violetlastname@gmail.com>
2024-01-10 14:22:20 -0500

    fix vertical window resizing not working

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,8 +6,8 @@ local opts = {
 -- resize windows w/ arrow keys
 vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
 vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
-vim.keymap.set('n', '<C-Left>', ':vertical -2<CR>', opts)
-vim.keymap.set('n', '<C-Right>', ':vertical +2<CR>', opts)
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
 
 -- increment/decrement tab level (but betterer)
 vim.keymap.set('v', '<', '<gv', opts)
