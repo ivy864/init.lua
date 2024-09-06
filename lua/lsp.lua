84babc63e8832ce80f7049dcfd539d1b4074a701
vayers <violetlastname@gmail.com>
2024-09-06 12:30:48 -0400

    setup nvim-lsp, add "enabled" field to mason lsp setup

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -47,19 +47,19 @@ local on_attach = function(_, bufnr)
     }
 end
 
-
-
 local servers = {
     clangd = {
-
+        enabled = true,
     },
     lua_ls = {
+        enabled = true,
         Lua = {
             workspace = { checkThirdParty = false },
             telemetry = { enable = false },
         },
     },
     pyright = {
+        enabled = true,
         python = {
             analysis = {
                 autoSearchPaths = true,
@@ -70,9 +70,10 @@ local servers = {
         }
     },
     jdtls = {
-        root_dir = vim.fs.root(0, {".git", "mvnw", "gradlew"})
+        enable = false,
     }
 }
+
 
 require('neodev').setup()
 
@@ -82,16 +83,19 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
+    --automatic_installation = { exclude = {"jdtls"} },
     ensure_installed = vim.tbl_keys(servers),
 }
 
 mason_lspconfig.setup_handlers {
     function(server_name)
-        require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = servers[server_name],
-            filetypes = (servers[server_name] or {}).filetypes,
-        }
+        if server_name.enable then
+            require('lspconfig')[server_name].setup {
+                capabilities = capabilities,
+                on_attach = on_attach,
+                settings = servers[server_name],
+                filetypes = (servers[server_name] or {}).filetypes,
+            }
+        end
     end,
 }
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -53,6 +53,7 @@ require("lazy").setup({
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-path',
         },
+
     },
 
     {
@@ -135,9 +136,69 @@ require("lazy").setup({
     {
         'aznhe21/actions-preview.nvim',
     },
+    {
+        'mfussenegger/nvim-jdtls',
+        ft = {"java"},
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+
+            local config = {
+                cmd = {
+                    -- 💀
+                    'java', -- or '/path/to/java17_or_newer/bin/java'
+                            -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+                    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+                    '-Dosgi.bundles.defaultStartLevel=4',
+                    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+                    '-Dlog.protocol=true',
+                    '-Dlog.level=ALL',
+                    '-Xmx1g',
+                    '--add-modules=ALL-SYSTEM',
+                    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+                    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+                    -- 💀
+                    '-jar', vim.fn.expand('~/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar'),
+                         -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+                         -- Must point to the                                                     Change this to
+                         -- eclipse.jdt.ls installation                                           the actual version
+
+
+                    -- 💀
+                    '-configuration', vim.fn.expand('~/.local/share/nvim/mason/packages/jdtls/config_linux'),
+                    --'/path/to/jdtls_install_location/config_SYSTEM',
+                                    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+                                    -- Must point to the                      Change to one of `linux`, `win` or `mac`
+                                    -- eclipse.jdt.ls installation            Depending on your system.
+
+
+                    -- 💀
+                    -- See `data directory configuration` section in the README
+                    '-data', vim.fn.expand('~/.cache/jdtls/workspace') .. project_name
+                    --'/path/to/unique/per/project/workspace/folder'
+                },
+
+                root_dir = vim.fs.root(0, {".git", "mvnw", "gradlew"}),
+
+                settings = {
+                    java = {}
+                },
+                init_options = {
+                    bundles = {}
+                },
+                capabilities = capabilities,
+            }
+
+            require("jdtls").start_or_attach(config)
+        end,
+
+    }
 })
 
 require("config/actions-preview")
 require("config/nvim-cmp")
 require("config/treesitter")
 require("config/telescope")
+--require("config/jdtls")
