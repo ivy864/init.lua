d5cba071c23e4d1c08d3db1ef6a1ca2cd16b6bd9
vayers <violetlastname@gmail.com>
2025-09-24 15:10:09 -0400

    change on_attach to autocmd

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,71 +1,81 @@
 require('mason').setup()
 require('mason-lspconfig').setup()
 
-local on_attach = function(_, bufnr)
-    local nmap = function(keys, func, desc)
-        if desc then
-            desc = 'LSP: ' .. desc
+vim.diagnostic.enable = true
+vim.diagnostic.config({
+    virtual_text = true,
+})
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(event)
+        local nmap = function(keys, func, desc)
+            if desc then
+                desc = 'LSP: ' .. desc
+            end
+
+           vim.keymap.set('n', keys, func, { buffer = event.buf, desc = desc})
         end
 
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc})
+        nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+        --nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+        nmap('<leader>ca', require("actions-preview").code_actions, '[C]ode [A]ction')
+
+        nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+        nmap('gr', require('telescope.builtin').lsp_references, '[G]oto References')
+        nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'type [D]efinition')
+        nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+        nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+        nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+        nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+        nmap('gD', vim.lsp.buf.definition, '[G]oto [D]eclaration')
+        nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+        nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+        nmap('<leader>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+
+        end, '[W]orkspace [L]ist Folders')
+
+        nmap('<leader>of', function()
+            vim.diagnostic.open_float()
+        end, '[O]pen [F]loat')
+        vim.api.nvim_buf_create_user_command(event.buf, 'Format', function(_)
+            vim.lsp.buf.format()
+        end, {desc = 'Format current buffer with LSP'})
+
+        --[[
+        require('which-key').register {
+            ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+            ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+            ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+            ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+            ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+            ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+            ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+            ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        }
+        --]]
+        require('which-key').add {
+            { "<leader>c", group = "[C]ode" },
+            { "<leader>c_", hidden = true },
+            { "<leader>d", group = "[D]ocument" },
+            { "<leader>d_", hidden = true },
+            { "<leader>g", group = "[G]it" },
+            { "<leader>g_", hidden = true },
+            { "<leader>h", group = "Git [H]unk" },
+            { "<leader>h_", hidden = true },
+            { "<leader>r", group = "[R]ename" },
+            { "<leader>r_", hidden = true },
+            { "<leader>s", group = "[S]earch" },
+            { "<leader>s_", hidden = true },
+            { "<leader>t", group = "[T]oggle" },
+            { "<leader>t_", hidden = true },
+            { "<leader>w", group = "[W]orkspace" },
+            { "<leader>w_", hidden = true },
+        }
     end
-
-    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    --nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-    nmap('<leader>ca', require("actions-preview").code_actions, '[C]ode [A]ction')
-
-    nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto References')
-    nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-    nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'type [D]efinition')
-    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-    nmap('gD', vim.lsp.buf.definition, '[G]oto [D]eclaration')
-    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-    nmap('<leader>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, '[W]orkspace [L]ist Folders')
-
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-    end, {desc = 'Format current buffer with LSP'})
-
-    --[[
-    require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-    }
-    --]]
-    require('which-key').add {
-        { "<leader>c", group = "[C]ode" },
-        { "<leader>c_", hidden = true },
-        { "<leader>d", group = "[D]ocument" },
-        { "<leader>d_", hidden = true },
-        { "<leader>g", group = "[G]it" },
-        { "<leader>g_", hidden = true },
-        { "<leader>h", group = "Git [H]unk" },
-        { "<leader>h_", hidden = true },
-        { "<leader>r", group = "[R]ename" },
-        { "<leader>r_", hidden = true },
-        { "<leader>s", group = "[S]earch" },
-        { "<leader>s_", hidden = true },
-        { "<leader>t", group = "[T]oggle" },
-        { "<leader>t_", hidden = true },
-        { "<leader>w", group = "[W]orkspace" },
-        { "<leader>w_", hidden = true },
-    }
-end
+})
 
 local servers = {
     clangd = {
